d0c2a10591f5a65d9c8fbab365c90b638a980e62
Garima Gupta <garima.gupta@appinventiv.com>
2023-04-14 17:04:02 +0530

    Update

--- src/screens/detail.js/index.js
+++ src/screens/detail.js/index.js
@@ -34,9 +34,13 @@ function Details() {
   }, []);
   return (
     <div>
-      <Title image="4.jpeg" title="How I met your mother" rating="4.5 - 2021" />
+      <Title
+        image={detailData?.primaryImage?.url}
+        title={detailData?.titleText?.text}
+        rating="4.5 - 2021"
+      />
       <div className="details-wrapper">
-        <Description desText="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum." />
+        <Description desText={"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."} />
         {detailData && detailData?.titleText ? (
           <Episode
             episodeTitle={detailData?.titleText?.text}
@@ -46,21 +50,6 @@ function Details() {
         ) : (
           ""
         )}
-        {/* <Episode
-          episodeTitle="Episode 2"
-          episodeImg="4.jpeg"
-          episodeDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-        />
-        <Episode
-          episodeTitle="Episode 3"
-          episodeImg="4.jpeg"
-          episodeDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-        />
-        <Episode
-          episodeTitle="Episode 4"
-          episodeImg="4.jpeg"
-          episodeDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-        /> */}
       </div>
     </div>
   );
