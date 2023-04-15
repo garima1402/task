887ee1d7ccd544c1307d02d334c70060305159a2
Garima Gupta <garima.gupta@appinventiv.com>
2023-04-15 11:00:02 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./screens";
-import Card from "./screens/card/card";
+import Card from "./screens/card";
 import Header from "./screens/header";
 
 export default function App() {
